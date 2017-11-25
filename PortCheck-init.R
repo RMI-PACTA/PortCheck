df82a6794bb27cdad83fe146e46c98b90a89c51a
tricia2dii <tricia@2degrees-investing.org>
2017-11-24 21:32:29 -0500

    Updated to work with global 2dii-init.R

--- PortCheck-init.R
+++ PortCheck-init.R
@@ -1,8 +1,19 @@
 
+### Set this Variable
+PROJ.NAME <- "PortCheck"
 
-ALIGNMENT.CODE.PATH <- paste0(PORTCHECK.CODE.PATH)
 
+### These are In here for backwards compatibility
+### Eventually should change the code to use the upper case constants
+DataFolder <- PROC.DATA.PATH
 FundDataLocation <- FUND.DATA.PATH
-DataFolder <- paste0(REF.DATA.PATH, "01_ProcessedData/")
 
+### Uncomment if you want to use logging
+### LOG.PATH <- paste0(PROJ.PATH,"SomeFolderWhereYouWriteLogs/")
+### source(paste0(COMMON.CODE.PATH,"/log.R"))
+### init.log()
 
+print(paste0("*** ", PROJ.NAME, " Initialized ***"))
+consts <- grep("(PATH|NAME)", ls(), value=TRUE)
+consts.val <- unname(sapply(consts, function(x) eval(parse(text=x))))
+PROJ.CONSTS <- data.frame(Constant=consts, Location=consts.val)
