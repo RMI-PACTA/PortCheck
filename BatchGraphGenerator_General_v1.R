0b397962b9bb69d5cbcb1252584d12f67e42cde0
tinaGNAW <tina@2degrees-investing.org>
2018-04-13 09:21:48 -0700

    produce latex

--- BatchGraphGenerator_General_v1.R
+++ BatchGraphGenerator_General_v1.R
@@ -212,7 +212,7 @@ unique(intersect(EQBatchTest$Scenario, EQCompProdSnapshots$Scenario))
 # Loop through Portfolios
 #--------
 
-for (i in c(326)){
+for (i in c(1:10,326)){
 
   ### Specify the Names from the Test List
   
