83e452bcde9a9428f9261e1955c79ce3e539b066
tinaGNAW <tina@2degrees-investing.org>
2018-05-15 12:06:05 -0700

    。

--- GraphFunctions.R
+++ GraphFunctions.R
@@ -2188,7 +2188,7 @@ Oilshare <- function(plotnumber, companiestoprint, ChartType){
       coord_flip()+
       theme(legend.position = "bottom",legend.title = element_blank(),
             plot.margin = unit(c(1, 6, 0, 0), "lines"), axis.line.x = element_line(colour = textcolor,size=0.5))+
-       guides(fill = guide_legend(ncol = 5,keywidth=1))+
+       guides(fill = guide_legend(ncol = 5,keywidth=1))
       # annotation_custom(
       #   grob = textGrob(label = "Weight", hjust =-0.9,gp=gpar(fontsize=textsize,col=textcolor)),
       #   xmin = n_distinct(OilCompanies$Name)+0.5, xmax = n_distinct(OilCompanies$Name)+1, ymin = 1, ymax = 1.05)
